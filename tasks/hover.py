701f956ca895fdbe0bc979d4fa3a6f3306e21e6b
Dmytro Bagaiev <dmytro.bagaiev@tomra.com>
2018-04-14 23:00:09 +0200

    Adjust Hover task reward function

--- tasks/hover.py
+++ tasks/hover.py
@@ -29,18 +29,21 @@ class Hover(Task):
     def get_reward(self):
         reward = 20.0
 
-        # Compute reward for reaching target height
-        target_z_distance = abs(self.target_z - self.position[2])
-        height_penalty = target_z_distance
-
-        # Penalize for shifting in horizontal plane
-        xy_shift_penalty = math.sqrt(self.position[0]**2 + self.position[1]**2)
-
         def apply_lower_threshold(x, threshold):
             return max(0.0, x - threshold)
 
         def apply_higher_threshold(x, threshold):
             return max(0.0, threshold - x)
+
+        # Compute reward for reaching target height
+        target_z_distance = abs(self.target_z - self.position[2])
+        height_penalty = target_z_distance ** 0.7
+
+        # Penalize for shifting in horizontal plane
+        xy_shift_penalty = math.sqrt(self.position[0]**2 + self.position[1]**2)
+
+        # Penalize for going to far away
+        too_far_penalty = apply_lower_threshold(target_z_distance, 120) ** 2
 
         # Penalise for twist angles
         twist_penalty = apply_lower_threshold((1. - np.cos(self.orientation)).sum(), 0.1)
@@ -49,7 +52,7 @@ class Hover(Task):
         rotation_penalty = apply_lower_threshold(np.abs(self.sim.angular_v).sum(), 0.5)
 
         # Orientation reward
-        reward -= height_penalty * 0.05 + xy_shift_penalty * 0.01 + twist_penalty * 3.0 + rotation_penalty * 0.5
+        reward -= height_penalty * 1.0 + xy_shift_penalty * 0.01 + too_far_penalty * 0.1 + twist_penalty * 3.0 + rotation_penalty * 0.5
 
         # Target closeness reward
         reward += apply_higher_threshold(target_z_distance, 5.0) * 10
