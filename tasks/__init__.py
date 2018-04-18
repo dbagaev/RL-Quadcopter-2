7a76c9602bb13ed81bdf3f434d1cf99ccedf47a2
Dmytro Bagaiev <dmytro.bagaiev@tomra.com>
2018-04-18 22:56:10 +0200

    Adopt emulator code to be compatible with new one

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,2 +1,5 @@
 from .takeoff import Takeoff
 from .hover import Hover
+
+from .emulator.takeoff import Takeoff as EmulatorTakeoff
+from .emulator.hover import Hover as EmulatorHover
--- tasks/emulator/emulator.py
+++ tasks/emulator/emulator.py
@@ -1,5 +1,8 @@
 from geometry_msgs.msg import Vector3, Point, Quaternion, Pose, Twist, Wrench
 
+import numpy as np
+
+
 def limit(t, mn, mx):
     if t < mn:
         return mn
@@ -9,44 +12,41 @@ def limit(t, mn, mx):
         return t
 
 class Emulator:
-    def __init__(self):
-        self.pose = Pose(
-            position=Point(0.0, 0.0, 0.0),  # drop off from a slight random height
-            orientation=Quaternion(0.0, 0.0, 0.0, 0.0))
-        self.velocity = Twist(
-            linear=Vector3(0.0, 0.0, 0.0),
-            angular=Vector3(0.0, 0.0, 0.0)
-        )
+    def __init__(self, init_pose=None, init_v=None, runtime=10.):
+        self.init_pose = init_pose
+        self.init_velocities = init_v
+        self.runtime = runtime
+        self.reset()
+
+    def reset(self):
+        self.pose = np.array([0.0, 0.0, 10.0, 0.0, 0.0, 0.0]) if self.init_pose is None else np.copy(self.init_pose)
+        self.v = np.array([0.0, 0.0, 0.0])
+        self.angular_v = np.array([0.0, 0.0, 0.0])
+
+        self.linear_accel = np.array([0.0, 0.0, 0.0])
+        self.angular_accels = np.array([0.0, 0.0, 0.0])
+        self.prop_wind_speed = np.array([0., 0., 0., 0.])
+        self.done = False
 
         self.time = 0.0
         self.time_delta = 0.1
-        self.gravity = 10.0
+        self.gravity = np.array([0.0, 0.0, 9.8])
 
-    def reset(self, pose, velocity):
-        self.pose = pose
-        self.velocity = velocity
-        self.time = 0.0
+    def next_timestep(self, w):
+        w[0] = limit(w[0], -25.0, 25.0)
+        w[1] = limit(w[1], -25.0, 25.0)
+        w[2] = limit(w[2], -25.0, 25.0)
 
-    def process(self, w):
-        w.force.x = limit(w.force.x, -25.0, 25.0)
-        w.force.y = limit(w.force.y, -25.0, 25.0)
-        w.force.z = limit(w.force.z, -25.0, 25.0)
+        self.pose[0:3] = self.pose[0:3] + self.v * self.time_delta + 0.5 * (w - self.gravity) * self.time_delta * self.time_delta
+        self.v += (w - self.gravity) * self.time_delta
 
-        self.velocity.linear.x += w.force.x * self.time_delta
-        self.velocity.linear.y += w.force.y * self.time_delta
-        self.velocity.linear.z += (w.force.z - self.gravity) * self.time_delta
-
-        self.pose.position.x += self.velocity.linear.x * self.time_delta
-        self.pose.position.y += self.velocity.linear.y * self.time_delta
-        self.pose.position.z += self.velocity.linear.z * self.time_delta
-
-        if(self.pose.position.z < 0):
-            self.pose.position.z = 0
-            self.velocity.linear.z = 0
+        # At the ground?
+        if self.pose[2] < 0:
+            self.pose[2] = 0
+            self.v[2] = 0
 
         self.time += self.time_delta
 
-    def dump(self):
-        print('#{:4.1f}: [{:9.3f}, {:9.3f}, {:9.3f}]'.format(self.time, self.pose.position.x, self.pose.position.y, self.pose.position.z), end=' ')
-        pass
+        if self.time > self.runtime:
+            self.done = True
 
